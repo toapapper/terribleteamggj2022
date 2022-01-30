913852c42f1970bfa9017333e070d19f3e4dd45a
Zippsson <62258314+Zippsson@users.noreply.github.com>
2022-01-30 13:23:37 +0100

    Updates to main menu prefab + Button sounds

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -11,6 +14,28 @@ public class MainMenu : MonoBehaviour
     GameObject options;
     [SerializeField]
     GameObject buttons;
+
+    [SerializeField] AudioMixer mixer;
+    private void Awake()
+    {
+        DelayedUpdateVolume();
+    }
+
+    async void DelayedUpdateVolume()
+    {
+        await Task.Delay(10);
+        UpdateMixer();
+    }
+
+    private void UpdateMixer()
+    {
+        float masterValue = PlayerPrefs.GetFloat("MasterVolume");
+        float musicValue = PlayerPrefs.GetFloat("MusicVolume");
+        float sfxValue = PlayerPrefs.GetFloat("SFXVolume");
+        mixer.SetFloat("MasterVolume", Mathf.Log10(masterValue) * 30f);
+        mixer.SetFloat("MusicVolume", Mathf.Log10(musicValue) * 30f);
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sfxValue) * 30f);
+    }
 
     public void StartGame()
     {
