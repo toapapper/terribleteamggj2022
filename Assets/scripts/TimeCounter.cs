658242a4f62f772fdd8af299ddd2c9e4ec552475
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2022-02-02 20:54:42 +0100

    Fixed and added timer to all levels, fixed levelselect scene

--- Assets/scripts/TimeCounter.cs
+++ Assets/scripts/TimeCounter.cs
@@ -10,6 +10,12 @@ public class TimeCounter : MonoBehaviour
 
     private void Update()
     {
-        timeText.text = (Mathf.Round(GameManager.Instance.GetTimer() * 100f) * 0.01f).ToString();
+        //timeText.text = (Mathf.Round(GameManager.Instance.GetTimer() * 100f) * 0.01f).ToString();
+        int minutes  = (int)GameManager.Instance.GetTimer() / 60;
+        int seconds  = (int)GameManager.Instance.GetTimer() % 60;
+        int fraction  = (int)(GameManager.Instance.GetTimer() * 1000) % 1000;
+
+        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+        
     }
 }
